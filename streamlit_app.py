d3280911cdacf4a2d59dde2a42a21581e4bbe61d
LaserMHP <laser.karasahin@mhp.com>
2025-07-29 13:50:42 +0200

    Update streamlit_app.py
    Connected changes needed bc of move from SiS to SniS

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
+import pandas as pd
+import numpy as ny
+#from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 
@@ -10,6 +12,8 @@ st.write(
   """Choose the fruits you want in your Smoothie!
   """)
 
+cnx=st.connection("snowflake")
+session = cnx.session
 
 
 name_on_order = st.text_input("Name on Smoothie:")
